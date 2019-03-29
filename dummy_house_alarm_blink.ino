3b7c6fdbd66d01132180a44fd5dd2e5bfca155ec
Tom Sparrow <sones857@users.noreply.github.com>
2019-03-29 19:28:38 +0000

    Update dummy_house_alarm_blink.ino

--- dummy_house_alarm_blink.ino
+++ dummy_house_alarm_blink.ino
@@ -2,14 +2,13 @@
  to blink 2 LED's alternatly every 10 seconds, for a dummy house alarm
 */
 
-void setup()
-{
+void setup(){
 pinMode(5,OUTPUT);
 pinMode(6,OUTPUT);
 }
 
-void loop()
-{
+
+void loop(){
 digitalWrite(5, HIGH);
 delay(1500);
 digitalWrite(5,LOW);
